c92ecc479e1de80dbef9b82ef1539cc19bc08ae5
iMac <torv.device027@passmail.net>
2025-10-06 13:56:33 +0700

    aa

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,19 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y gcc kernel-devel make patch wget kernel-headers tar
 # install the kernel headers
-sudo dnf install kernel-devel
 
-# clone the repo of the module
-git clone https://github.com/egorenar/snd-hda-codec-cs8409.git
-# make and install
-cd snd-hda-codec-cs8409
-make
-make install
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
